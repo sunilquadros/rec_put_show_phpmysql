06132dc0db62ecbe3c966a65e1ebc423cb8880f9
sunilquadros <sunilquadros@workstation.lab.example.com>
2017-07-19 06:13:23 -0400

    first commit

---
+++ file2.php
@@ -0,0 +1,55 @@
+<?php
+//$servername = "172.30.251.114";
+//$username = "ose";
+//$password = "openshift";
+//$dbname = "quotes";
+// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($_ENV["MYSQLDB_SERVICE_HOST"],"instructor","openshift","instructor",
+ $_ENV["MYSQLDB_SERVICE_PORT"])
+     or die("Error " . mysqli_error($conn));
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+echo " \n";
+$sql = "SELECT instructorNumber, instructorName, email, city, state, postalCode, country
+    from instructors";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+   // output data of each row
+   while($row = $result->fetch_assoc()) {
+        //  echo "InNum:- " . $row["instructorNumber"]. " " ;
+        //  echo "InName:- " . $row["instructorName"]. " " ;
+        //  echo "Email:- " . $row["email"]. " " ;
+        //  echo "City:- " . $row["city"]. " " ;
+        //  echo "State:- " .  $row["state"]. " " ;
+        //  echo "PostalCode:- " . $row["postalCode"]. " " ;
+        //  echo "Country:- " . $row["country"]. " " ;
+        //echo " \n";
+         //  $id = $row["id"];
+        $instructorNumber = $row["$instructorNumber"];
+        $instructorName =  $row["instructorName"];
+        $email = $row["email"];
+        $city =  $row["city"];
+        $state = $row["state"];
+        $postalCode =  $row["postalCode"];
+        $country = $row["country"];
+        echo "  <div style='margin:30px 0px;'>
+        // Id: $id<br />
+           Instructor Number: $instructorNumber<br />
+           Instructor Name: $instructorName<br />
+           Email: $email<br />
+           City: $city<br />
+           State: $state<br />
+           Postal Code: $postalCode<br />
+           Country: $country<br />
+           </div>
+           ";
+   }
+} else {
+   echo "0 results";
+}
+$conn->close();
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+label{display:inline-block;width:100px;margin-bottom:10px;}
+</style>
+
+
+<title>Add Record</title>
+</head>
+  
+<body>
+
+<form method="post" action="file1.php">
+<label>Instructor Number</label>
+<input type="text" name="instructorNumber"  />
+<br />
+<label>Instructor Name</label>
+<input type="text" name="instructorName"  />
+<br />
+<label>Email</label>
+<input type="text" name="email" />
+<br />
+<label>City</label>
+<input type="text" name="city" />
+<br />
+<label>State</label>
+<input type="text" name="state"  />
+<br />
+<label>Postal Code</label>
+<input type="text" name="postalCode" />
+<br />
+<label>Country</label>
+<input type="text" name="country" />
+
+<br />
+<input type="submit" value="Add Record">
+</form>
+
+
+
+</body>
+</html>
